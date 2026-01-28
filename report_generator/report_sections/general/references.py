0932e37cb0ce024394ca7c16770d8ccf3822982a
SaraLMS <slmsantos01@gmail.com>
2026-01-28 02:06:46 +0000

    wrote references constants

---
+++ report_generator/report_sections/general/references.py
@@ -0,0 +1,88 @@
+########## EU-OSHA ################
+REF_1 = ("Agência Europeia para a Segurança e Saúde no Trabalho. (2021). Postura sentada e estática por períodos "
+         "prolongados no trabalho: Efeitos na saúde e conselhos de boas práticas (Resumo). Serviço das Publicações da "
+         "nião Europeia.")
+LINK_1 = "https://osha.europa.eu/pt/publications/postura-sentada-e-estatica-por-periodos-prolongados-no-trabalho-efeitos-na-saude-e-conselhos-de-boas-praticas-resumo"
+
+REF_1_1 = "European Agency for Safety and Health at Work. (2017, February 9). Ergonomics in office work. OSHwiki."
+LINK_1_1 = "https://oshwiki.osha.europa.eu/en/themes/ergonomics-office-work"
+
+########## WHO ###################
+REF_2 = "World Health Organization. (2020). WHO guidelines on physical activity and sedentary behaviour. World Health Organization."
+LINK_2 = "https://www.who.int/publications/i/item/9789240015128"
+
+REF_3 = "World Health Organization. (2022). WHO guidelines on mental health at work. World Health Organization."
+LINK_3 = "https://www.who.int/publications/i/item/9789240053052"
+
+############## GUIA DGES ##########
+
+REF_4 = ("Direção-Geral da Saúde. (2021). Guia técnico n.º 3: Vigilância da saúde dos trabalhadores expostos a "
+          "fatores de risco psicossocial no local de trabalho (Versão síntese). Ministério da Saúde, Programa Nacional de Saúde Ocupacional.")
+LINK_4 = "https://www.sgeconomia.gov.pt/destaques/dgs-guia-tecnico-vigilancia-da-saude-dos-trabalhadores-expostos-a-fatores-de-risco-psicossocial-no-local-de-trabalho.aspx"
+
+############# OiRA tool ################
+
+REF_5 = "European Agency for Safety and Health at work (EU-OSHA), Online interactive Risk Assessment (OiRA), 2019. [Online]. Disponível em:"
+
+LINK_5 = 'https://oiraproject.eu/en'
+
+####### Ergo Office PhD thesis ###########
+
+REF_6 = ("Lima, T. (2013). Ergo@Office: Uma metodologia de identificação de fatores de risco orientada para a "
+         "prevenção das lesões músculo-esqueléticas relacionadas com o trabalho: Estudo de caso de funcionários "
+         "administrativos no sector público sob uma perspetiva de género (Tese de doutoramento, Universidade da Beira Interior).")
+
+LINK_6 = "https://ubibliorum.ubi.pt/entities/publication/fa920aaf-e5d5-428e-913e-b43590d224f0"
+
+########## ROSA ###########
+
+REF_7 = ("Sonne, M., Villalta, D. L., & Andrews, D. M. (2012). Development and evaluation of an office ergonomic risk "
+         "checklist: Rapid Office Strain Assessment (ROSA). Applied Ergonomics, 43(1), 98–108.")
+LINK_7 = "https://www.sciencedirect.com/science/article/abs/pii/S0003687011000433?via%3Dihub"
+
+
+################## NOISE ##############################
+REF_8 = ("Sun, Z., Hu, S., Xie, S., Wu, L., Jiang, C., Ding, S., Zhang, Z., Xu, W., & Li, H. (2025). Does background "
+         "sound impact cognitive performance and relaxation states in enclosed office? Building and Environment, 267, 112313.")
+
+LINK_8 = "https://doi.org/10.1016/j.buildenv.2024.112313"
+
+REF_9 = ("Jahncke, H. (2012). Cognitive performance and restoration in open-plan office noise (Doctoral dissertation, "
+         "Luleå University of Technology). Luleå University of Technology.")
+
+LINK_9 = "https://www.sciencedirect.com/science/article/abs/pii/S0272494411000429?via%3Dihub"
+
+############## PSYCHOSOCIAL/stress #############
+
+REF_10 = ("Knight, C., & Haslam, S. A. (2010). The relative merits of lean, enriched, and empowered offices: an experimental "
+          "examination of the impact of workspace management strategies on well-being and productivity. Journal of "
+          "experimental psychology. Applied, 16(2), 158–172.")
+
+LINK_10 = "https://doi.org/10.1037/a0019292"
+
+REF_11 = "Luijcks R, Hermens HJ, Bodar L, Vossen CJ, Os Jv, et al. (2014) Experimentally Induced Stress Validated by EMG Activity. PLOS ONE 9(4): e95215."
+LINK_11 = "https://doi.org/10.1371/journal.pone.0095215"
+
+########## COPSOQ, MUEQ ##########################
+REF_12 = ("Cotrim, T. P., Bem-Haja, P., Pereira, A., Fernandes, C., Azevedo, R., Antunes, S., Pinto, J. S., Kanazawa, F., "
+          "Souto, I., Brito, E., & Silva, C. F. (2022). The Portuguese Third Version of the Copenhagen Psychosocial "
+          "Questionnaire: Preliminary Validation Studies of the Middle Version among Municipal and Healthcare Workers. "
+          "International Journal of Environmental Research and Public Health, 19(3), 1167.")
+
+LINK_12 = "https://doi.org/10.3390/ijerph19031167"
+
+REF_13 = ("Turci, A. M., Bevilaqua-Grossi, D., Pinheiro-Araujo, C. F., Bragatto, M. M., & Chaves, T. C. (2015). The "
+          "Brazilian Portuguese version of the revised Maastricht Upper Extremity Questionnaire (MUEQ-Br revised): "
+          "Translation, cross-cultural adaptation, reliability, and structural validation. BMC Musculoskeletal Disorders, 16, Article 497.")
+
+LINK_13 = "https://doi.org/10.1186/s12891-015-0497-2"
+
+
+# ------------------------------------------------------------------------------------------------------------------- #
+# lists
+# ------------------------------------------------------------------------------------------------------------------- #
+
+REFS_LIST = [REF_1, REF_1_1, REF_2, REF_3, REF_4, REF_5, REF_6, REF_7, REF_8, REF_9, REF_10, REF_11, REF_12, REF_13]
+LINKS_LIST = [LINK_1, LINK_1_1, LINK_2, LINK_3, LINK_4, LINK_5, LINK_6, LINK_7, LINK_8, LINK_9, LINK_10, LINK_11, LINK_12, LINK_13]
+
+
